0d2f32136855bb009156bb4eb769989b45555347
ti <tidaybreak@gmail.com>
2022-04-14 14:19:29 +0800

    Create suikanma.js

---
+++ suikanma.js
@@ -0,0 +1,12 @@
+/*
+README：https://github.com/yichahucha/surge/tree/master
+ */
+
+const path1 = "serverConfig";
+const path2 = "wareBusiness";
+const path3 = "basicConfig";
+const url = $request.url;
+const body = $response.body;
+
+let obj = JSON.parse(body);
+$done({ body: JSON.stringify(obj) });
